f59d79dfbaa025d72a69efb64cf3aa47a128500f
piotr286 <piotr286@gmail.com>
2019-03-20 01:52:00 +0100

    AAA

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -1,15 +1,22 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
 
 /**
  * Created by jt on 5/24/17.
  */
 
+@Controller
 public class PropertyInjectedController {
 
-    public GreetingServiceImpl greetingService;
+    @Autowired
+    @Qualifier("greetingServiceImpl")
+    public GreetingService greetingService;
 
     public String sayHello(){
         return greetingService.sayGreeting();
--- src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -2,10 +2,14 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 5/24/17.
  */
+@Controller
 public class SetterInjectedController {
     private GreetingService greetingService;
 
@@ -13,6 +17,8 @@ public class SetterInjectedController {
         return greetingService.sayGreeting();
     }
 
+    @Autowired
+    @Qualifier("getterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
