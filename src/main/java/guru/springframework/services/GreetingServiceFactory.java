2299f49167630025bdc538df360f220ff70c534d
piotr286 <piotr286@gmail.com>
2019-03-28 02:21:19 +0100

    AAA

---
+++ src/main/java/guru/springframework/services/GreetingServiceFactory.java
@@ -0,0 +1,29 @@
+package guru.springframework.services;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+public class GreetingServiceFactory {
+
+    private GreetingRepository greetingRepository;
+
+    public GreetingServiceFactory(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
+    public GreetingService createGreetingService(String lang) {
+        switch (lang) {
+            case "en":
+                return new PrimaryGreetingService(greetingRepository);
+            case "de:":
+                return new PrimaryGermanGreetingService(greetingRepository);
+            case "es":
+                return new PrimarySpanishGreetingService(greetingRepository);
+            default:
+                return new PrimaryGreetingService(greetingRepository);
+        }
+    }
+
+
+}
